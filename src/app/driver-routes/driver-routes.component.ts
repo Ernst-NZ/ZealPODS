98187d61b17aa63f1adb9a2abf9d2718fd8e8199
Ernst Visser <ernst@hotmail.co.nz>
2018-09-01 16:50:53 +1200

    Before Index

--- src/app/driver-routes/driver-routes.component.ts
+++ src/app/driver-routes/driver-routes.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DataService} from '../data.service';
-import {trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+import {trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-driver-routes',
@@ -9,26 +9,27 @@ import {trigger, style, transition, animate, keyframes, query, stagger } from '@
   animations: [
     trigger('listStagger', [
       transition('* <=> *', [
-        query(':enter',
+        query(':enter', 
         [
-          style({ opacity: 0, transform: 'translateY(-10px)'}),
+          style({ opacity: 0, transform: 'translateY(-15px)'}),
           stagger('50ms',
           animate('550ms ease-out',
-          style({opacity: 1, transform: 'translateY(0px)'})))
+          style({opacity:1, transform: 'translateY(0px)'})))
         ], {optional: true}),
         query(':leave', animate('50ms', style({ opacity: 0 })), {optional: true})
       ])
     ])
-  ]
+  ]    
 })
 export class DriverRoutesComponent implements OnInit {
 
   allRoutes$: object;
+  
   constructor(private data: DataService) { }
 
 
   ngOnInit() {
     this.data.getAllRoutes().subscribe(
-      data => this.allRoutes$ = data);
+      data => this.allRoutes$ = data)
   }
 }
