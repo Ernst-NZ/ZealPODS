fa186d77cc1b6efcba11d68ccd0d835a69e350a6
Ernst-NZ <ernst@hotmail.co.nz>
2018-10-30 08:51:17 +1300

    Fix errrors on Data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -52,31 +52,31 @@ export class DataService {
 
 
   // ######## 44  JQuery  #######
-  postJson4() {
-    var person = {
-      name: $("#id-name").val(),
-      address: $("#id-address").val(),
-      phone: $("#id-phone").val()
-    }
+  // postJson4() {
+  //   var person = {
+  //     name: $("#id-name").val(),
+  //     address: $("#id-address").val(),
+  //     phone: $("#id-phone").val()
+  //   }
 
-    $('#target').html('sending..');
-    console.log("44 Jquery");
-    console.log(JSON.stringify(person));
-    $.ajax({
-      url: 'https://test1.zealsystems.co.nz/api/values',
-      type: 'post',
-      //dataType: 'json',
-      //contentType: 'application/json',
-      //    Authorization: 'Bearer aRA39yf4N8J-DhTNCVRXptKywROwFCi6meGgAhafP9SGy96ReXmnwZYNTkqA_-vHcYOtNLgk0wCKTw1gkFfWj8S8A6j073XCcHeqlW7R-ahC3jOQ70iRcINbOsVsUWHqQ-YRi5u6bUZz9ECWruJ6vsIymMqBj9Lvk839h-9-F3iKQjOvXBmIA1j3ymGpO-FeuiXeQ3OnGxuUjWCCg6faVARQplX00GjoEgW1fgOJkvOxqgGlV5zWFUxfYh-oMlRfLSx7z4ZSm-OCKEmI5CIV1zzFodwp6n_mMrWy5AeOD0cu2mdDHBlqEp1E72s16Wi2liVW0dCcmoozZyJ0lhEy4SD-3_tkjW3-ShlZdCZ1niaIL5D6NBXeN1YeLFiEPGwYnmTV2j6f6PM9jUKCR9EWDODJejgVV8iDyAGFHuNDaO5IInQSzLyDCPmRq3QNeCTRuFV4tB0CJ1_Okj8wxkFdRg0jqU3o9oFq5_szZ47sV3tz-U2lIb0OFXl4kz-oWDpz'
-      //    ,
-      data: JSON.stringify(person)
-    }).done(function () {
-      alert("second success");
-    })
-      .fail(function (e) {
-        console.log(e)
-        alert(e);
-      });
+  //   $('#target').html('sending..');
+  //   console.log("44 Jquery");
+  //   console.log(JSON.stringify(person));
+  //   $.ajax({
+  //     url: 'https://test1.zealsystems.co.nz/api/values',
+  //     type: 'post',
+  //     //dataType: 'json',
+  //     //contentType: 'application/json',
+  //     //    Authorization: 'Bearer aRA39yf4N8J-DhTNCVRXptKywROwFCi6meGgAhafP9SGy96ReXmnwZYNTkqA_-vHcYOtNLgk0wCKTw1gkFfWj8S8A6j073XCcHeqlW7R-ahC3jOQ70iRcINbOsVsUWHqQ-YRi5u6bUZz9ECWruJ6vsIymMqBj9Lvk839h-9-F3iKQjOvXBmIA1j3ymGpO-FeuiXeQ3OnGxuUjWCCg6faVARQplX00GjoEgW1fgOJkvOxqgGlV5zWFUxfYh-oMlRfLSx7z4ZSm-OCKEmI5CIV1zzFodwp6n_mMrWy5AeOD0cu2mdDHBlqEp1E72s16Wi2liVW0dCcmoozZyJ0lhEy4SD-3_tkjW3-ShlZdCZ1niaIL5D6NBXeN1YeLFiEPGwYnmTV2j6f6PM9jUKCR9EWDODJejgVV8iDyAGFHuNDaO5IInQSzLyDCPmRq3QNeCTRuFV4tB0CJ1_Okj8wxkFdRg0jqU3o9oFq5_szZ47sV3tz-U2lIb0OFXl4kz-oWDpz'
+  //     //    ,
+  //     data: JSON.stringify(person)
+  //   }).done(function () {
+  //     alert("second success");
+  //   })
+  //     .fail(function (e) {
+  //       console.log(e)
+  //       alert(e);
+  //     });
 
     // $.post('https://test1.zealsystems.co.nz/api/values', JSON.stringify(person), function (data) {
     //   console.log('success callback');
@@ -90,7 +90,7 @@ export class DataService {
     //     console.log('failed');
     //   });
     // console.log("45 Jquery");
-  }
+ // }
 
 
 
@@ -124,5 +124,3 @@ export class DataService {
 
 // ########################################################################
 }
-
-
