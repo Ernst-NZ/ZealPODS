eff6fd9aa35033d6084c7a552d345e8f3b13da51
Ernst-NZ <ernst@hotmail.co.nz>
2019-01-11 16:31:06 +1300

    sdad

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -7,8 +7,8 @@ import { User } from 'src/app/_models/user';
 export class UserService {
     constructor(private http: HttpClient) { }
 
-    getAll() {
-      //  return this.http.get<User[]>('https://deliveryapi.completefoodservices.com.au:8095/api/values/1');
-        return this.http.get<User[]>(`https://test1.zealsystems.co.nz/api/values/1`);
-    }
+    // getAll() {
+    //   //  return this.http.get<User[]>('https://deliveryapi.completefoodservices.com.au:8095/api/values/1');
+    //     return this.http.get<User[]>(`https://test1.zealsystems.co.nz/api/values/1`);
+    // }
 }
